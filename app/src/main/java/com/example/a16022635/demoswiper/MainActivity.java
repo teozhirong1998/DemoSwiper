c6b84a751602f77655e48cfdd91665452014d071
teozhirong1998 <16022635@myrp.edu.sg>
2018-07-24 12:15:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022635/demoswiper/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.a16022635.demoswiper;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        vPager = (ViewPager) findViewById(R.id.viewpager1);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+    }
+}
