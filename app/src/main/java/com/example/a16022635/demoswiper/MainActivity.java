8e88c57c0ff4f8a5414c738d23973dae0e0ce182
teozhirong1998 <16022635@myrp.edu.sg>
2018-07-24 12:24:05 +0800

    Initial commit

--- app/src/main/java/com/example/a16022635/demoswiper/MainActivity.java
+++ app/src/main/java/com/example/a16022635/demoswiper/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -12,13 +14,15 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Fragment> al;
     MyFragmentPagerAdapter adapter;
     ViewPager vPager;
+    Button btnBack, btnNext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         vPager = (ViewPager) findViewById(R.id.viewpager1);
-
+        btnNext = (Button) findViewById(R.id.btnNext);
+        btnBack = (Button) findViewById(R.id.btnBack);
         FragmentManager fm = getSupportFragmentManager();
 
         al = new ArrayList<Fragment>();
@@ -28,5 +32,26 @@ public class MainActivity extends AppCompatActivity {
         adapter = new MyFragmentPagerAdapter(fm, al);
 
         vPager.setAdapter(adapter);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(vPager.getCurrentItem() > 0) {
+                    int previousPage = vPager.getCurrentItem() - 1;
+                    vPager.setCurrentItem(previousPage, true);
+                }
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int max = vPager.getChildCount();
+                if(vPager.getCurrentItem() < max-1) {
+                    int nextPage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextPage, true);
+                }
+            }
+        });
     }
 }
